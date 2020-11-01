80a58c18312898b522c7810c03470287552dd7f6
mattb0101 <mattbaum666@googlemail.com>
2020-11-01 16:16:30 +0000

    Bind data from db to accordion

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 
@@ -96,7 +96,7 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
-    
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
